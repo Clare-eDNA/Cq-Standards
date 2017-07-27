443e2b0159ce0ec995d7c85c246dff0ef8a93846
Turdlez <ciadams@iastate.edu>
2017-07-27 12:37:28 -0500

    FINALLY got graphs to work

--- Munging and graph code.R
+++ Munging and graph code.R
@@ -18,12 +18,19 @@ library(dplyr)
 #plot(Cq~Dil, Bl, xlab="Dilution Factor", ylab="Cq Value",abline(line), col="red", untf=TRUE))
 
 #Blood standards plot
-ggplot(Bl, aes(x = Dil, y = Cq)) + geom_point() + geom_smooth(method='loess', color="blue", se=F) +xlab("Dilution")+ylab("Cq Value")
+ggplot(Bl, aes(x = Dil, y = Cq)) + geom_point(size=6)+ geom_smooth(method='loess', color="blue", se=F) +xlab("Dilution")+ylab("Cq value")+theme_classic(base_size = 11, base_family = "")+theme(axis.title.x = element_text(size=60),
+       axis.text.x  = element_text(vjust=0.5, size=50),
+       axis.title.y = element_text(size=60),
+       axis.text.y= element_text(vjust=0.5, size=50))
+  
+  
 fit <- lm(Bl$Cq~log(Bl$Dil)) #code for a logistic regression of fit
 summary(fit) #spits out the results
 
 #eDNA standards plot
-ggplot(eDNA, aes(x = Dil, y = Cq)) + geom_point() + geom_smooth(method='loess', color="blue",se=F) +xlab("Dilution")+ylab("Cq Value")
+ggplot(eDNA, aes(x = Dil, y = Cq)) + geom_point(size=6) + geom_smooth(method='loess', color="blue",se=F) +xlab("Dilution")+ylab("Cq Value")+theme_classic(base_size = 11, base_family = "")+theme(axis.title.x = element_text(size=50),                                  axis.text.x  = element_text(vjust=0.5, size=35),                                        axis.title.y = element_text(size=40),                                                   axis.text.y= element_text(vjust=0.5, size=35))
+
+
 fit <- lm(Bl$Cq~log(Bl$Dil)) #code for a logistic regression of fit
 summary(fit) #spits out the results
 
